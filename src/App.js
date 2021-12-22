e6ac967385d869aee19736dbe7c4a8d96f06ddb1
Tim <timothy.sereda@trojans.dsu.edu>
2021-12-22 16:44:26 -0600

    broke it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react"
 import generator from "sudoku"
+import SudokuBoard from "./components/SudokuBoard"
 import "./App.css"
 
 window.generator = generator
 
+
+function generateSudoku() {
+  const raw = generator.makepuzzle()
+
+  const result = {rows: []}
+
+  for (let i=0; i<9; i++){
+    const row = {cols: [], index: i}
+    for (let j = 0; j<9; j++ ) {
+      const value = raw[i*9+j]
+      const col = {
+        row: i,
+        col: j,
+        value: value,
+        readonly: value !== null
+      };
+      row.cols.push(col);
+    }
+    result.rows.push(row);
+  }
+
+  return result;
+}
+
 class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    sudoku: generateSudoku()
+  }
+}
+
   render() {
     return (
       <div className="App">
@@ -13,6 +45,7 @@ class App extends Component {
             Liam's Nuts
           </h1>
         </header>
+        <SudokuBoard sudoku={this.state.sudoku}/>
       </div>
     )
   }
